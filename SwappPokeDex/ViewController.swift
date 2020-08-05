80f6f8489ca832f3dc0e20553229955c69ab26f1
Colton Swapp <coltonbswapp@gmail.com>
2020-08-04 20:55:18 -0600

    second commit

---
+++ SwappPokeDex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwappPokeDex
+//
+//  Created by Colton Swapp on 8/4/20.
+//  Copyright © 2020 Colton Swapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
